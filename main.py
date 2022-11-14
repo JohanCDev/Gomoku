8ba8ea42862a4e4c501032a4c7abb4b5cf7680cf
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-11-14 13:56:36 +0100

    add: base code

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+import src.Game
+
+if __name__ == '__main__':
+    sys.exit(src.Game.run())
---
+++ src/Game.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def run() -> int:
+    print("Gomoku")
+    return 0
