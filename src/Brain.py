57fb2eb093b80f52924333900d1e84d79ad54fe8
Nathan <nathan.rousseau@epitech.eu>
2022-11-22 14:25:39 +0100

    begin of refactor of the AI #40

---
+++ src/Brain.py
@@ -0,0 +1,148 @@
+from src.GomokuBoard import pawnType
+from src.GomokuBoard import *
+from src.utils.PrintGomoku import print_gomoku
+import random
+
+
+PAWN_DOWN = True
+PAWN_UP = False
+
+WIN = 5
+NAIVE = 4
+EXPLORE = 3
+
+NONE = -1
+COLUMN = 1
+LINE = 2
+DIAGONAL_LEFT = 3
+DIAGONAL_RIGHT = 4
+
+class Brain:
+
+    def __init__(self, boardSize):
+        self.board = GomokuBoard(boardSize)
+        self.boardSize = boardSize
+
+    def __check_lines(self, nb_align, line_to_check, searched_pawn_type):
+        nb: int = 0
+        first : int = 0
+        y : int = 0
+        for y in range(0, len(line_to_check)):
+            if line_to_check[y] == searched_pawn_type:
+                if nb == 0:
+                    first = y
+                nb += 1
+            else:
+                nb = 0
+            if nb == nb_align and nb_align == WIN:
+                return True, 0
+            elif nb == nb_align:
+                rightFree = True
+                leftFree = True
+                if y + 1 < len(line_to_check):
+                    if line_to_check[y + 1] != searched_pawn_type and line_to_check[y + 1] != pawnType.EMPTY:
+                        rightFree = False
+                if first - 1 >= 0:
+                    if line_to_check[first - 1] != searched_pawn_type and line_to_check[first - 1] != pawnType.EMPTY:
+                        leftFree = False
+                if rightFree:
+                    return True, y + 1
+                elif leftFree:
+                    return True, first - 1
+                else:
+                    nb = 0
+            y += 1
+        return False, -1
+
+    def __check_align(self, nb_align : int, pawn_type_to_check: pawnType):
+
+        # def check_on_column(y: int):
+        #     nb: int = 0
+        #     for x in range(0, self.__boardSize - 1):
+        #         if self.__boardManager.get_pawn(x, y) == pawn_type_to_check:
+        #             nb += 1
+        #         else:
+        #             nb = 0
+        #         if nb == nb_alignn:
+        #             return True, x
+        #     return False, -1
+
+        # def check_diagonals():
+        #     RIGHT = 1
+        #     LEFT = -1
+        #     def check_one_diagonal(i, j, direct : int) -> bool:
+        #         aligned : bool = True
+
+        #         for k in range(0, nb_alignn):
+        #             if not self.__boardManager.boardMap[i + k][j + (k * direct)] == pawn_type_to_check:
+        #                 aligned = False
+        #         return aligned, i, j
+
+        #     for i in range(self.__boardSize):
+        #         for j in range(len(self.__boardManager.boardMap[i])):
+        #             if i + (nb_alignn -1) < self.__boardSize and j + (nb_alignn -1) < len(self.__boardManager.boardMap[i]):
+        #                 found, x, y = check_one_diagonal(i, j, RIGHT)
+        #                 if found:
+        #                     return DIAGONAL_RIGHT, x, y
+        #             if i + (nb_alignn -1) < self.__boardSize and j - (nb_alignn -1) >= 0:
+        #                 found, x, y = check_one_diagonal(i, j, LEFT)
+        #                 if found:
+        #                     return DIAGONAL_LEFT, x, y
+        #     return NONE, -1, -1
+
+        x = 0
+        for line in self.board.boardMap:
+            found, y = self.__check_lines(nb_align, line, pawn_type_to_check)
+            if found:
+                print_gomoku(f'{x} {y}')
+                return LINE, x, y
+            x += 1
+
+        # for i in range(0, self.__boardSize - 1):
+        #     found, x = check_on_column(i)
+        #     if found:
+        #         return COLUMN, x, i
+        # found, x, y  = check_diagonals()
+        # if found != NONE:
+        #     return found, x, y
+        return NONE, -1, -1
+
+    def __get_random_coords(self, max_value: int):
+        rand_x = random.randrange(max_value)
+        rand_y = random.randrange(max_value)
+        while self.board.get_pawn(rand_x, rand_y) != pawnType.EMPTY:
+            rand_x = random.randrange(max_value)
+            rand_y = random.randrange(max_value)
+        return rand_x, rand_y
+
+    def __naive_thinking(self):
+        aligned, x, y = self.__check_align(NAIVE, pawnType.BRAIN)
+        if aligned != NONE:
+            print_gomoku(f'{x} {y}')
+            return PAWN_DOWN, x, y
+
+        aligned, x, y = self.__check_align(NAIVE, pawnType.MANAGER)
+        if aligned != NONE:
+            return PAWN_DOWN, x, y
+        return PAWN_UP
+
+    def check_win(self, pawn_type_to_check : pawnType) -> bool:
+        res, _, _ = self.__check_align(WIN, pawn_type_to_check)
+        return res != NONE
+
+    def act(self, force_random : bool = True):
+        x = 0
+        y = 0
+        if force_random:
+            x, y = self.__get_random_coords(self.boardSize - 1)
+            while self.board.get_pawn(x, y) != pawnType.EMPTY:
+                x, y = self.__get_random_coords(self.boardSize - 1)
+            self.board.add_brain_pawn(x, y)
+            return x, y
+        else:
+            action, x, y = self.__naive_thinking()
+            if action == PAWN_UP:
+                self.act(True)
+            else:
+                self.board.add_brain_pawn(x, y)
+                return x, y
--- src/Game.py
+++ src/Game.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
-import random
 from src.GomokuBoard import *
 from src.ParseInput import ParseInput
 from src.Globals import *
 from src.utils.PrintGomoku import print_gomoku
-
-
-WIN = 5
-NAIVE = 4
-EXPLORE = 3
-
-NONE = -1
-COLUMN = 1
-LINE = 2
-DIAGONAL_LEFT = 3
-DIAGONAL_RIGHT = 4
-
-PLACED = True
-NOT_PLACED = False
+from src.Brain import Brain
 
 class Game:
     def __init__(self):
         self.parser = ParseInput()
-        self.__boardManager = GomokuBoard(20)
         self.command_map = {
             "BOARD": self.board_command,
             "START": self.start_command,
@@ -32,79 +17,7 @@ class Game:
             "END": self.end_command,
             "ABOUT": self.about_command
         }
-        self.__boardSize = 20
-
-    def __get_random_coords(self, max_value: int):
-        rand_x = random.randrange(max_value)
-        rand_y = random.randrange(max_value)
-        while self.__boardManager.get_pawn(rand_x, rand_y) != pawnType.EMPTY:
-            rand_x = random.randrange(max_value)
-            rand_y = random.randrange(max_value)
-        return rand_x, rand_y
-
-    def __check_align(self, nb_alignn : int, pawn_type_to_check: pawnType):
-        def check_on_line(line_to_check: list[pawnType]) -> bool:
-            nb: int = 0
-            currY : int = 0
-            for char in line_to_check:
-                if char == pawn_type_to_check:
-                    nb += 1
-                else:
-                    nb = 0
-                if nb == nb_alignn:
-                    return True, currY
-                currY += 1
-            return False, -1
-
-        def check_on_column(y: int):
-            nb: int = 0
-            for x in range(0, self.__boardSize - 1):
-                if self.__boardManager.get_pawn(x, y) == pawn_type_to_check:
-                    nb += 1
-                else:
-                    nb = 0
-                if nb == nb_alignn:
-                    return True, x
-            return False, -1
-
-        def check_diagonals():
-            RIGHT = 1
-            LEFT = -1
-            def check_one_diagonal(i, j, direct : int) -> bool:
-                aligned : bool = True
-
-                for k in range(0, nb_alignn):
-                    if not self.__boardManager.boardMap[i + k][j + (k * direct)] == pawn_type_to_check:
-                        aligned = False
-                return aligned, i, j
-
-            for i in range(self.__boardSize):
-                for j in range(len(self.__boardManager.boardMap[i])):
-                    if i + (nb_alignn -1) < self.__boardSize and j + (nb_alignn -1) < len(self.__boardManager.boardMap[i]):
-                        found, x, y = check_one_diagonal(i, j, RIGHT)
-                        if found:
-                            return DIAGONAL_RIGHT, x, y
-                    if i + (nb_alignn -1) < self.__boardSize and j - (nb_alignn -1) >= 0:
-                        found, x, y = check_one_diagonal(i, j, LEFT)
-                        if found:
-                            return DIAGONAL_LEFT, x, y
-            return NONE, -1, -1
-
-        x = 0
-        for line in self.__boardManager.boardMap:
-            found, y = check_on_line(line)
-            if found:
-                return LINE, x, y
-            x += 1
-
-        for i in range(0, self.__boardSize - 1):
-            found, x = check_on_column(i)
-            if found:
-                return COLUMN, x, i
-        found, x, y  = check_diagonals()
-        if found != NONE:
-            return found, x, y
-        return NONE, -1, -1
+        self.__brain = Brain(20)
 
     def run(self) -> int:
         while True:
@@ -114,62 +27,6 @@ class Game:
             except KeyError:
                 print_gomoku("UNKNOWN message - command ",
                              self.parser.get_parsed_input()[0], "not existing.")
-
-    def __naive_place_pawn(self, alignment, x, y : int) -> bool:
-        placed : bool = False
-        """ FAIRE DE L'AUTRE Côté aussi !!!!!"""
-        if alignment == LINE:
-            if y + 1 < self.__boardSize:
-                self.__boardManager.add_brain_pawn(x, y + 1)
-                placed = True
-            if not placed and y - 4 > -1:
-                self.__boardManager.add_brain_pawn(x, y - 4)
-                placed = True
-        if alignment == COLUMN:
-            if x + 1 < self.__boardSize:
-                self.__boardManager.add_brain_pawn(x + 1, y)
-                placed = True
-        if alignment == DIAGONAL_RIGHT:
-            if y + 4 < self.__boardSize and x + 4 < self.__boardSize:
-                self.__boardManager.add_brain_pawn(x + 4, y + 4)
-                placed = True
-        if alignment == DIAGONAL_LEFT:
-            if y + 4 < self.__boardSize and x - 4 > -1:
-                self.__boardManager.add_brain_pawn(x + 4, y - 4)
-                placed = True
-
-        return placed
-
-    def naive_thinking(self):
-        """ BEGIN """
-        aligned, x, y = self.__check_align(NAIVE, pawnType.MANAGER)
-        if aligned != NONE:
-            if self.__naive_place_pawn(aligned, x, y):
-                return PLACED
-
-        aligned, x, y = self.__check_align(NAIVE, pawnType.BRAIN)
-        if aligned != NONE:
-            if self.__naive_place_pawn(aligned, x, y):
-                return PLACED
-        return NOT_PLACED
-
-
-    def brain_thinking(self, force_random : bool = False):
-        x = 0
-        y = 0
-        if force_random:
-            x, y = self.__get_random_coords(self.__boardSize - 1)
-            while self.__boardManager.get_pawn(x, y) != pawnType.EMPTY:
-                x, y = self.__get_random_coords(self.__boardSize - 1)
-            self.__boardManager.add_brain_pawn(x, y)
-        else:
-            if self.naive_thinking() == NOT_PLACED:
-                self.brain_thinking(True)
-
-        win, _, _ = self.__check_align(WIN, pawnType.BRAIN)
-        if win != NONE:
-            print_gomoku("Message message - I've win !")
-            self.end_command()
 
     def start_command(self) -> bool:
         board_size: int = -1
@@ -182,10 +39,10 @@ class Game:
         except ValueError:
             print_gomoku("ERROR message - unsupported size or other error")
             return False
-        self.__boardSize = board_size
+        self.__brain.boardSize = board_size
         print_gomoku("OK - everything is good")
-        self.__boardManager.reset_board(self.__boardSize)
-        print_gomoku(self.__boardManager)
+        self.__brain.board.reset_board(self.__brain.boardSize)
+        print_gomoku(self.__brain.board)
         return True
 
     def turn_command(self) -> bool:
@@ -196,24 +53,27 @@ class Game:
                 print_gomoku("ERROR message - Unauthorized move")
                 return False
             for arg in parsed_args:
-                if int(arg) >= self.__boardSize or int(arg) < 0:
+                if int(arg) >= self.__brain.boardSize or int(arg) < 0:
                     print_gomoku(
                         "ERROR message - Unauthorized move (invalid position)")
                     return False
             print_gomoku("DEBUG message - Valid TURN command")
-            if self.__boardManager.get_pawn(int(parsed_args[0]), int(parsed_args[1])) != pawnType.EMPTY:
+            if self.__brain.board.get_pawn(int(parsed_args[0]), int(parsed_args[1])) != pawnType.EMPTY:
                 print_gomoku("ERROR message - This cell is already taken")
                 return False
             else:
-                self.__boardManager.add_manager_pawn(
+                self.__brain.board.add_manager_pawn(
                     int(parsed_args[0]), int(parsed_args[1]))
 
-            win, _, _ = self.__check_align(WIN, pawnType.MANAGER)
-            if win != NONE:
+            if self.__brain.check_win(pawnType.MANAGER):
                 print_gomoku("Message message - You've win...")
                 self.end_command()
 
-            self.brain_thinking()
+            self.__brain.act()
+
+            if self.__brain.check_win(pawnType.BRAIN):
+                print_gomoku("Message message - I've win !!")
+                self.end_command()
 
         except IndexError:
             print_gomoku("ERROR message - No movement was given")
@@ -224,11 +84,11 @@ class Game:
         except RuntimeError:
             print_gomoku("ERROR message - Runtime error of getPawn")
             return False
-        print_gomoku(self.__boardManager)
+        print_gomoku(self.__brain.board)
         return True
 
     def begin_command(self) -> bool:
-        self.brain_thinking(True)
+        self.__brain.act(True)
         print_gomoku("DEBUG message - Valid BEGIN command")
         return True
 
@@ -243,9 +103,9 @@ class Game:
                 x = int(pawnPos[0])
                 y = int(pawnPos[1])
                 player = int(pawnPos[2])
-                if x < 0 or x >= self.__boardSize:
+                if x < 0 or x >= self.__brain.boardSize:
                     raise ValueError
-                if y < 0 or y >= self.__boardSize:
+                if y < 0 or y >= self.__brain.boardSize:
                     raise ValueError
                 if player not in [1, 2]:
                     raise ValueError
@@ -253,9 +113,9 @@ class Game:
                 print_gomoku("DEBUG Message - Failure on Board Input")
                 return False
             if player == 1:
-                self.__boardManager.add_brain_pawn(x, y)
+                self.__brain.board.add_brain_pawn(x, y)
             else:
-                self.__boardManager.add_manager_pawn(x, y)
+                self.__brain.board.add_manager_pawn(x, y)
             print_gomoku("DEBUG Message - Succes on Board Input")
             return True
 
@@ -316,7 +176,7 @@ class Game:
         return True
 
     def end_command(self) -> bool:
-        print_gomoku(self.__boardManager)
+        print_gomoku(self.__brain.board)
         exit(0)
 
     def about_command(self) -> bool:
