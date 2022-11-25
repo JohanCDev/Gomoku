cf18a92dde51d843921cd4eeba8d7a5be2f948e4
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-11-25 13:41:35 +0100

    fix: tests with x and y changed

--- tests/CheckAlign.py
+++ tests/CheckAlign.py
@@ -22,6 +22,7 @@ def test_basic_diagonal_win_situation():
     print(move, x, y)
     assert move == True
 
+
 def test_basic_diagonal_left_win_situation():
     game = Game()
     game.get_board_manager().reset_board(20)
@@ -31,11 +32,16 @@ def test_basic_diagonal_left_win_situation():
     game.get_board_manager().boardMap[3][1] = pawnType.BRAIN
     game.get_brain().boardSize = 20
     game.get_board_manager().boardMap = [
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN, pawnType.EMPTY],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY],
-        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.BRAIN],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.BRAIN, pawnType.EMPTY],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN,
+            pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
     ]
     game.get_brain().boardSize = 20
     move, x, y = game.get_brain().call_naive()
@@ -46,29 +52,40 @@ def test_basic_line_win_situation():
     game = Game()
     game.get_board_manager().reset_board(5)
     game.get_board_manager().boardMap = [
-        [pawnType.BRAIN, pawnType.EMPTY, pawnType.BRAIN, pawnType.BRAIN, pawnType.BRAIN],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN, pawnType.EMPTY],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.BRAIN, pawnType.EMPTY, pawnType.BRAIN,
+            pawnType.BRAIN, pawnType.BRAIN],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.BRAIN, pawnType.EMPTY],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN,
+            pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
     ]
     game.get_brain().boardSize = 5
     game.get_board_manager().__boardSize = 5
     move, x, y = game.get_brain().call_naive()
-    assert move == True and x == 0 and y == 1
+    print("MOVE", x, y)
+    assert move == True and x == 1 and y == 0
 
 
 def test_basic_col_win_situation():
     game = Game()
     game.get_board_manager().reset_board(5)
     game.get_board_manager().boardMap = [
-        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN],
-        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.BRAIN, pawnType.EMPTY],
-        [pawnType.BRAIN, pawnType.EMPTY, pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY],
-        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY],
-        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.BRAIN],
+        [pawnType.EMPTY, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.BRAIN, pawnType.EMPTY],
+        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
+        [pawnType.BRAIN, pawnType.EMPTY, pawnType.EMPTY,
+            pawnType.EMPTY, pawnType.EMPTY],
     ]
     game.get_brain().boardSize = 5
     game.get_board_manager().__boardSize = 5
     move, x, y = game.get_brain().call_naive()
-    assert move == True and x == 1 and y == 0
+    assert move == True and x == 0 and y == 1
